69970b217426af8329288af9ede84d64987381fd
Daniel Sousa <daniel.sousa@msn.com>
2024-05-15 22:28:17 -0300

    feat: ajuste de base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  // site: "https://danieliraja.github.io",
-  // base: "projeto-scareware-treinamento-cerus",
+  site: "https://danieliraja.github.io",
+  base: "/projeto-scareware-treinamento-cerus/",
   integrations: [tailwind()],
 });
